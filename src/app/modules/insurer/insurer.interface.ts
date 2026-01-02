81131306de45a9dfd1ff9947236c738d8e4b3322
rashidsarkar <rashidrock558@gmail.com>
2026-01-02 15:56:22 +0600

    `Removed unused code and commented out routes in insurer module`

--- src/app/modules/insurer/insurer.interface.ts
+++ src/app/modules/insurer/insurer.interface.ts
@@ -1,13 +1,34 @@
-import { Types } from "mongoose";
+import { Types } from 'mongoose';
+
+export type PolicyType =
+  | 'Comprehensive'
+  | 'Comprehensive Basic'
+  | 'Third Party Fire & Theft'
+  | 'Third Party Property Damage'
+  | 'Other / Not sure';
+
+export type ComplaintMade = 'no' | 'yesWithInsurer' | 'yesWithAfca';
+export type InsurerStatus = 'underReview' | 'reportReady' | 'failed';
 
 export interface IInsurer {
-    user: Types.ObjectId;
-    name: string;
-    username?: string;
-    phone?: string;
-    email: string;
-    address?: string;
-    profile_image?: string;
-    totalAmount?: number;
-    totalPoint?: number;
+  normalUserId: Types.ObjectId;
+
+  insurerName: string;
+  policyType: PolicyType;
+
+  incidentDate: Date;
+  firstNotifiedDate: Date;
+
+  incidentDescription: string;
+  insurerResponse?: string;
+  userConcern?: string;
+
+  complaintMade: ComplaintMade;
+  complaintStatus?: string;
+
+  supporting_Documents?: string[];
+  report_Document?: string;
+
+  failureNote?: string;
+  status: InsurerStatus;
 }
--- src/app/modules/insurer/insurer.routes.ts
+++ src/app/modules/insurer/insurer.routes.ts
@@ -1,25 +1,25 @@
-import express from "express";
-import auth from "../../middlewares/auth";
-import { USER_ROLE } from "../user/user.constant";
-import validateRequest from "../../middlewares/validateRequest";
-import insurerValidations from "./insurer.validation";
-import insurerController from "./insurer.controller";
-import { uploadFile } from "../../helper/fileUploader";
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
+import validateRequest from '../../middlewares/validateRequest';
+import insurerValidations from './insurer.validation';
+import insurerController from './insurer.controller';
+import { uploadFile } from '../../helper/fileUploader';
 
 const router = express.Router();
 
-router.patch(
-    "/update-profile",
-    auth(USER_ROLE.user),
-    uploadFile(),
-    (req, res, next) => {
-        if (req.body.data) {
-            req.body = JSON.parse(req.body.data);
-        }
-        next();
-    },
-    validateRequest(insurerValidations.updateInsurerData),
-    insurerController.updateUserProfile
-);
+// router.patch(
+//     "/update-profile",
+//     auth(USER_ROLE.user),
+//     uploadFile(),
+//     (req, res, next) => {
+//         if (req.body.data) {
+//             req.body = JSON.parse(req.body.data);
+//         }
+//         next();
+//     },
+//     validateRequest(insurerValidations.updateInsurerData),
+//     insurerController.updateUserProfile
+// );
 
 export const insurerRoutes = router;
